7d053e5d5b8db64ef8c57d109fcd1d9389e1ff59
Joiner2003 <joineredh@gmail.com>
2024-11-13 10:46:33 -0500

    Se añaden reglas de validacion

---
+++ app/Http/Requests/ProductoRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        $rules = [
+            'nombre' => 'required|string|max:255',
+            'descripcion' => 'required|string',
+            'precio' => 'required|numeric|min:0',
+            'stock' => 'required|integer|min:0',
+            'codigo' => 'required|string|unique:productos,codigo,' . $this->producto?->id,
+            'active' => 'boolean'
+        ];
+
+        return $rules;
+    }
+}
